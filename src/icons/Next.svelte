0eaea3ea0eb7ed24d3c141501365de3533805e59
Greg Marlow <gregm01@live.com>
2020-10-05 03:41:20 +0100

    fix icon colours

--- src/icons/Next.svelte
+++ src/icons/Next.svelte
@@ -1,3 +1,8 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+<svg
+  stroke="currentColor"
+  fill="currentColor"
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 24 24"
+>
   <path d="M0 19v-14l12 7-12 7zm12 0v-14l12 7-12 7z" />
 </svg>
--- src/icons/Play.svelte
+++ src/icons/Play.svelte
@@ -1,3 +1,8 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+<svg
+  stroke="currentColor"
+  fill="currentColor"
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 24 24"
+>
   <path d="M2 24v-24l20 12-20 12z" />
 </svg>
